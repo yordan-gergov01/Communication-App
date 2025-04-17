954a436998c0edf5afe5ab07a01cfc21bd4a7847
yordan-gergov01 <yordansoftskills@gmail.com>
2025-04-17 16:54:31 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const { spawn } = require("child_process");
+const fs = require("fs");
+
+const numberFormatter = spawn("number_formatter", ["./dest.txt", "$", ","]);
+
+numberFormatter.stdout.on("data", (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+numberFormatter.stderr.on("data", (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+numberFormatter.on("close", (code) => {
+  if (code === 0) {
+    console.log("The file was read, processed and written successfully!");
+  } else {
+    console.log("Something bad happened!");
+  }
+});
+
+const fileStream = fs.createReadStream("./src.txt");
+fileStream.pipe(numberFormatter.stdin);
